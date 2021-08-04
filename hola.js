c3207aad5ec3f8d84db65f6d66ffa0779c5cc2d9
makoto2711 <45396379+makoto2711@users.noreply.github.com>
2021-08-03 22:37:43 -0500

    MEJORA PERFORMANCE DEL CODIGO
    nuevos cambios que se hicieron en la clase de hoy en Discord

--- hola.js
+++ hola.js
@@ -1,124 +1,101 @@
 (function() 
 {
-const $input = document.getElementById("input") 
-const $save = document.getElementById("save") 
-const $update = document.getElementById("update") 
-const $items = document.getElementById("items") 
-const $template = document.getElementById("template").content
+    "use strict"
+    const $input = document.getElementById("input") 
+    const $save = document.getElementById("save") 
+    const $update = document.getElementById("update") 
+    const $items = document.getElementById("items") 
+    const $template = document.getElementById("template").content
+    const fragmento = document.createDocumentFragment()
+    const p = document.createElement("p");
+    p.textContent = "No hay items nuevos 🐲";
+    p.classList.add("text-center");
 
-const p = document.createElement("p");
-p.textContent = "No hay items nuevos 🐲";
-p.classList.add("text-center");
+    let arr = [], jason = {};
+    let clonar
+    let pos_id = 0
+    let cambio = false
 
-const fragmento = document.createDocumentFragment()
- 
-let arr = [], jason = {};
-let clonar
-let save_id = 0
+    read_array(arr); 
 
-let boton = false
-
-revisar_arr(arr); 
-
-    window.addEventListener("keyup", (e)=> 
-    { 
-        if (e.key === "Enter") 
-        {
-            if ($input.value.trim() === "")  alert("ingresa un dato") 
-            else
-            {
-                if (boton) 
-                {
-                    arr[save_id].item = $input.value;
-                    $update.classList.add("d-none");
-                    $save.classList.remove("d-none"); 
-                }
-                else
-                {
-                        jason = { item: $input.value };
-                        arr.push(jason);
-                }
-                revisar_arr(arr); 
-            }
-
-        }
-    });
-
-
-    $save.addEventListener("click", ()=> 
-    {
-        if ($input.value.trim() === "")  alert("ingresa un dato")    
-        else
-        {
-            jason = { item: $input.value };
-            arr.push(jason);
-            revisar_arr(arr); 
-        }
-    });
- 
-
-    $update.addEventListener("click", ()=> 
-    {
-        if ($input.value.trim() === "")  alert("ingresa un dato")
-        else 
-        {
-            arr[save_id].item = $input.value;
-            $update.classList.add("d-none");
-            $save.classList.remove("d-none");
-            revisar_arr(arr);    
-        }
-    });
-
- 
-    // FUNCIONES
-    function revisar_arr(arr) 
-    {
-        $input.value = ""
-        $input.focus()
-        const longitud = arr.length
-
-        while ( $items.firstChild  )  $items.removeChild($items.firstChild);
-
-        if (longitud == 0) 
-        {
-            $items.appendChild(p);
-        } 
-        else
-        {       
-            boton = false
-            arr.forEach((item,i) => 
-                {
-                    clonar = $template.cloneNode(true)
-                    clonar.querySelector(".texto").textContent = item.item
-                    clonar.querySelector(".d-flex").id = i
-                    fragmento.appendChild(clonar)
-                });
-
-                $items.appendChild(fragmento);
-        } 
-    } 
-
+    $save.addEventListener("click", saveItems);
+    $update.addEventListener("click", updateItems);
 
     window.addEventListener("click", (e)=> 
     {
-        if (e.target.classList.contains("update")) 
-        { 
-            save_id = e.target.parentNode.parentNode.id;
-            $input.value = arr[e.target.parentNode.parentNode.id].item;
-            $save.classList.add("d-none");
-            $update.classList.remove("d-none");
-            boton = true   
-            $input.focus() 
-        }
-        else if(e.target.classList.contains("delete"))
+        if (e.target.classList.contains("delete")) 
         {
-            arr.splice(e.target.parentNode.parentNode.id, 1);
+            const id_item = e.target.parentNode.parentNode.id;
+            arr.splice( id_item , 1 );
             e.target.parentNode.parentNode.classList.add("dissapear");
-            setTimeout(() => 
-            {
-                revisar_arr(arr); 
-            }, 500);        
+            setTimeout(() => {  read_array(arr);  }, 500);
+        }
+        else if(e.target.classList.contains("update"))
+        {
+            const id_item = e.target.parentNode.parentNode.id;
+            pos_id = id_item;
+            $input.value = arr[id_item].item;
+
+            $update.classList.remove("d-none");
+            $save.classList.add("d-none");
+            cambio = true;
+            $input.focus();           
         }
     });
+
+
+    window.addEventListener("keyup", (e)=> 
+    {
+        if (e.key == "Enter") 
+        {
+            if (!cambio)  saveItems();    
+            else  updateItems(); 
+        } 
+    });
+
+    // FUNCIONES
+    function saveItems() 
+    {
+        if ($input.value.trim() === "")  alert("Ingresa un dato");    
+        else
+        {
+            jason = { item: $input.value }
+            arr.push(jason) 
+            console.log(arr);
+            read_array(arr);
+        }
+    }
+
+    function updateItems() 
+    {
+        arr[pos_id].item = $input.value
+        $update.classList.add("d-none")   
+        $save.classList.remove("d-none")   
+        read_array(arr);     
+    }
+
+    function read_array(arr) 
+    {
+        $input.value = "";
+        $input.focus();
+        cambio = false;
+        const longitud = arr.length;
+
+        while ( $items.firstChild  )  $items.removeChild($items.firstChild);
+
+        if (longitud == 0)  $items.appendChild(p); 
+        else
+        {       
+            arr.forEach((item,i) => 
+            {
+                clonar = $template.cloneNode(true)
+                clonar.querySelector(".texto").textContent = item.item
+                clonar.querySelector(".d-flex").id = i
+                fragmento.appendChild(clonar)
+            });
+
+            $items.appendChild(fragmento);
+        } 
+    } 
 
 })();
